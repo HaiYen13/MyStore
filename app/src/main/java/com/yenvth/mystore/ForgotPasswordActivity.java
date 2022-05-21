fc3a193426a501cc9fff981c387726adb4b603bf
HaiYen13 <haiyencute13@gmail.com>
2022-05-21 19:11:48 +0700

    initial commit

---
+++ app/src/main/java/com/yenvth/mystore/ForgotPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.yenvth.mystore;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
---
+++ app/src/main/java/com/yenvth/mystore/SignUpActivity.java
@@ -0,0 +1,24 @@
+package com.yenvth.mystore;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+    private TextView tvLogin;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+    }
+    private void initView(View view){
+
+    }
+
+}
